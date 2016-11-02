3b47a85ee23b2d04c58bdccdcad608c1c6f6d509
Jan Kuri <jkuri88@gmail.com>
2016-11-02 20:40:28 +0100

    chore: update deps

--- scripts/lib/spinner.ts
+++ scripts/lib/spinner.ts
@@ -1,11 +1,11 @@
-import * as ora from 'ora';
+import * as clispinner from 'cli-progress-spinner';
 import * as chalk from 'chalk';
 
 let spinner;
 
 export function start(msg = ''): void {
   if (spinner) { spinner.stop(); }
-  spinner = ora({
+  spinner = clispinner({
     text: chalk.yellow(msg),
     color: 'yellow',
     spinner: 'growVertical',
